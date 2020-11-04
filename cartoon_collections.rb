50758ca4578b0c935627f397694b46ee656f3428
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-04 15:47:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |element|
-    puts "Hello", " ", element
+    puts "Hello", " ", element "!"
   end
 end
 
@@ -11,4 +11,5 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  
 end
